e33d9cffb834212e113d063a628b8073f1b43c94
Veresek <filipowskipraca@gmail.com>
2024-10-17 16:54:05 +0200

    17.10v2

--- 17.10/2/js/script.js
+++ 17.10/2/js/script.js
@@ -1,13 +1,22 @@
 const p = document.getElementById('p');
-const inp = document.querySelector('input');
+const inp = document.querySelector('#inp');
 const btn = document.querySelector('button');
 const check = document.querySelector("input[type='checkbox']");
+check.addEventListener('change', () => {
+	if (check.checked) {
+		inp.disabled = true;
+	} else {
+		inp.disabled = false;
+	}
+});
 btn.addEventListener('click', () => {
 	if (check.checked) {
 		p.textContent = `Dowieziemy twoją pizze za darmo`;
 	} else {
-		p.textContent = `Dowóz będzie cię kosztował ${
-			Number(inp.value) * 2.5 * 2
-		} zł`;
+		p.textContent = `Dowóz będzie cię kosztował ${(
+			Number(inp.value) *
+			2.5 *
+			2
+		).toFixed(2)} zł`;
 	}
 });
