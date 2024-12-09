7928a3fbbd1350f4130f6e98e394bb7d197b5e1c
Veresek <filipowskipraca@gmail.com>
2024-12-09 11:31:01 +0100

    dawdads

---
+++ php/9.12/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>PHP</h1>
+    <?php
+    // $name = [6.3,2,"Adam",[1,2,3]];
+    // #echo "Witaj ".$name."<br>";
+    // var_dump($name);
+    // echo '<br>'.gettype($name)."<br>";
+    // echo '<pre>';
+    // print_r($name);
+    // echo '<pre>';
+    // define('PI', 3.14);
+    // define("email", "kacper.filipowksi.rpo@live.zs1mm.edu.pl");
+    // echo email;
+    // $num1 = 3;
+    // $num2 = 4;
+    // $result = $num1 % $num2;
+    // echo "<br>Modulo $num1 i $num2 to $result <br>";
+    // echo ++$num1.'<br>';
+    // $result +=5;
+    // echo $result;
+
+    <?php
+    echo "<h3>Klasa 2PRO</h3>";
+    echo "<a href='https://wp.pl' target='_blank'> Link do WP</a>"
+    ?>
+    ?>
+</body>
+</html>
