277329030f33d1e33b0d6f2a5b7968a08eacbae5
Veresek <filipowskipraca@gmail.com>
2025-02-19 10:35:10 +0100

    hgfhfgh

---
+++ php/19.02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+gdfgdfgd
+</body>
+</html>
+
+<?php 
+    $tab1 = [324,534,545,34,5];
+    print_r($tab1);
+?>
